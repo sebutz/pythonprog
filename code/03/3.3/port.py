6f92e804c9b769ac3713bf3008065fbdccb50be4
freemitive@gmx.com <freemitive@gmx.com>
2019-01-10 17:50:21 +0200

    Update port.py

--- code/03/3.3/port.py
+++ code/03/3.3/port.py
@@ -4,7 +4,7 @@ import csv
 
 total = 0.0
 
-with open('../../Data/portfolio.csv', 'r') as f:
+with open('../../Data/portfolio2.csv', 'r') as f:
     rows = csv.reader(f)
     headers = next(rows)   # Skip the header row
     for row in rows:
@@ -14,3 +14,16 @@ with open('../../Data/portfolio.csv', 'r') as f:
 
 print('Total cost:', total)
 
+
+# working on a higher level 
+# csv library is doing a lot of data preparation out-of-the-box
+total = 0.0
+with open('../../Data/portfolio.csv', 'r') as f:
+    rows = csv.reader(f)
+    headers = next(rows)   # Skip the header row
+    for row in rows:
+        row[2] = int(row[2])
+        row[3] = float(row[3])
+        total += row[2] * row[3]
+
+print('Total cost:', total)
